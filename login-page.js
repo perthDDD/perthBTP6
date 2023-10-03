c9ad113c2f4655b40605bd80cde96eb425de7b46
perthDDD <140643274+perthDDD@users.noreply.github.com>
2023-10-03 23:56:21 +0800

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -1,4 +1,4 @@
-³const loginForm = document.getElementById("login-form");
+const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
